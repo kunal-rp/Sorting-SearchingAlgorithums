ba2b095c3978ab3e29c528bfbd3e7fe2c5f1fb2e
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-06 14:40:39 -0700

    Added in the number Of Operations functionality ; tested and verified results

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -8,18 +8,23 @@ public class BubbleSort {
 	}
 	
 	public void sort(){
+		int numberOfOperations = 0;
 		for(int endIndex = numbers.length; endIndex >0; endIndex-- ){
-			
 			for(int startIndex = 0; startIndex <endIndex-1; startIndex++ ){
 				if(numbers[startIndex]>numbers[startIndex+1]){
 					int temp = numbers[startIndex+1];
 					numbers[startIndex+1] = numbers[startIndex];
 					numbers[startIndex] = temp;
+					numberOfOperations++;
 				}
+				
 			}
-			//print();
+			numberOfOperations++;
+			
 			
 		}
+		System.out.println("Number of Operations:"+ numberOfOperations);
+		
 	}
 	
 	public void print(){
