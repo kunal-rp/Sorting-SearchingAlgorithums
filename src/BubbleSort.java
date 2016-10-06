ad95afd6b2ab211daf2c2ed68051c05575d5313d
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-06 14:23:27 -0700

    last commit did not include changes , so I am adding changes to this one

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,4 +1,35 @@
 
 public class BubbleSort {
-
+	
+	int[] numbers;
+	
+	public BubbleSort(int[] numbers){
+		this.numbers = numbers;
+	}
+	
+	public int[] sort(){
+		for(int endIndex = numbers.length; endIndex >0; endIndex-- ){
+			
+			for(int startIndex = 0; startIndex <=endIndex-1; startIndex++ ){
+				if(numbers[startIndex]>numbers[startIndex+1]){
+					int temp = numbers[startIndex+1];
+					numbers[startIndex+1] = numbers[startIndex];
+					numbers[startIndex] = temp;
+				}
+			}
+			print();
+			
+		}
+		return numbers;
+	}
+	
+	public void print(){
+		for(int i = 0; i < numbers.length; i++){
+			System.out.print(numbers[i]+ " ");
+		}
+	}
+	
+	
+	
+	
 }
