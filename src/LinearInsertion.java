5ff7a7f5f1acf4e6dae46cc72b0e497b738cc43a
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-11 14:49:17 -0700

    Adding Number Of Operations Functionality to Bin Search

--- src/LinearInsertion.java
+++ src/LinearInsertion.java
@@ -9,17 +9,20 @@ public class LinearInsertion {
 	
 	
 	public void sort(){
-		
+		int numberOfOperations = 0;
 		for(int i = 0; i < numbers.length-1; i++){
 			int temp = numbers[i+1];
 			int j = i;
 			while(j >=0 && numbers[j]>temp){
 				numbers[j+1] = numbers[j];
 				j = j-1;
+				numberOfOperations++;
 			}
 			numbers[j+1] = temp;
+			numberOfOperations++;
 			
 		}
+		System.out.println("Number Of Operations: "+numberOfOperations);
 	}
 	
 	public void print(){
