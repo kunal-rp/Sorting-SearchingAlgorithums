44b4cd02fdae6712c9aa76b09c112897ae21f124
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-11 14:42:53 -0700

    Tested Bin Search Algor. Searching algorithum works changed main to go through both sorting algorithums and then search via user input

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -7,4 +7,24 @@ public class BinarySearch {
 		this.numbers = numbers;
 	}
 	
+	public int search(int startIndex, int endIndex, int numberWanted){
+		
+		int center =( endIndex+startIndex  )/ 2;
+		if(numbers[center] == numberWanted){
+			return center;
+		}
+		else if(startIndex != endIndex){
+			
+			if(numbers[center] < numberWanted){
+				return search(center+1, endIndex, numberWanted);
+			}
+			else if(numbers[center] > numberWanted){
+				return search(startIndex, center-1, numberWanted);
+			}
+		}
+		
+			return -1;
+		
+	}
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class Main {
@@ -8,28 +9,35 @@ public class Main {
 		int[] numbers;
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Size of array");
-		size = in.nextInt();
-		numbers = new int[size];
+		Random rand = new Random();
 		
-		for(int i = 0; i < size; i++){
-			System.out.print("Enter number:");
-			numbers[i] = in.nextInt();
+		int[] origional = new int[rand.nextInt(10)];
+		for(int i = 0; i < origional.length; i++){
+			origional[i] = (int) rand.nextInt(300);
 		}
 		
-		/*
+		int[] n1 = origional;
+		int[] n2 = origional;
+		
 		System.out.println("------------Bubble Sort--------------");
-		BubbleSort bs = new BubbleSort(numbers);
+		BubbleSort bs = new BubbleSort(n1);
 		bs.print();
 		bs.sort();
 		bs.print();
-		*/
+		
+		
 		
 		System.out.println("------------Linear Insertion Sort--------------");
-		LinearInsertion li = new LinearInsertion(numbers);
+		LinearInsertion li = new LinearInsertion(n2);
 		li.print();
 		li.sort();
 		li.print();
+		
+		
+		System.out.println("------------Binary search--------------");
+		BinarySearch bin_search = new BinarySearch(n1);
+		System.out.print("Number to search:");
+		System.out.println(bin_search.search(0, n1.length-1, in.nextInt()));
 
 	}
 
