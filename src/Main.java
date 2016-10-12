41f58b45095d874c6f6105a51f6c1b24aeb33bf7
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-11 17:46:11 -0700

    Fixed NumberOfOperations Bug on Lin Ins Changed main array input to randomize everytime new sorting array occurs

--- src/Main.java
+++ src/Main.java
@@ -16,28 +16,29 @@ public class Main {
 			origional[i] = (int) rand.nextInt(300);
 		}
 		
-		int[] n1 = origional;
-		int[] n2 = origional;
+		
 		
 		System.out.println("------------Bubble Sort--------------");
-		BubbleSort bs = new BubbleSort(n1);
+		BubbleSort bs = new BubbleSort(origional);
 		bs.print();
 		bs.sort();
 		bs.print();
 		
-		
+		for(int i = 0; i < origional.length; i++){
+			origional[i] = origional.length-i;//(int) rand.nextInt(300);
+		}
 		
 		System.out.println("------------Linear Insertion Sort--------------");
-		LinearInsertion li = new LinearInsertion(n2);
+		LinearInsertion li = new LinearInsertion(origional);
 		li.print();
 		li.sort();
 		li.print();
 		
 		
 		System.out.println("------------Binary search--------------");
-		BinarySearch bin_search = new BinarySearch(n1);
+		BinarySearch bin_search = new BinarySearch(origional);
 		System.out.print("Number to search:");
-		System.out.println(bin_search.search(0, n1.length-1, in.nextInt()));
+		System.out.println(bin_search.search(0, origional.length-1, in.nextInt()));
 
 	}
 
